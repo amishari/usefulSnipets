9a9575cc3b3b63c43fbba4636d7f18d851f37264
amishari <81028814+amishari@users.noreply.github.com>
2022-12-12 08:19:57 +0330

    finding duplicate items in an array

---
+++ findingDuplicatesArray.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 2, 4, 5, 5, 6];
+
+const set = new Set(numbers);
+
+const duplicates = numbers.filter(item => {
+    if (set.has(item)) {
+        set.delete(item);
+    } else {
+        return item;
+    }
+});
+
+console.log(duplicates);
