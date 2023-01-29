673f42a469c663994e37cea2b22b9cefe707d907
amishari <81028814+amishari@users.noreply.github.com>
2023-01-29 10:44:35 +0330

    Update duplicateItemsDisplay.js

--- duplicateItemsDisplay.js
+++ duplicateItemsDisplay.js
@@ -2,3 +2,6 @@ const DuplicateDisplay =(array) =>{
     array.filter((item,index)=>{
         index !== array.indexOf(item)});
     return array;}
+
+// finding number of occurrance in an array
+array.filter(x => x === element).length
