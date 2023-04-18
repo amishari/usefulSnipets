e62d031b02f505877ab4eb81fc182eaa23f40335
amishari <81028814+amishari@users.noreply.github.com>
2023-04-18 07:40:46 +0330

    Delete capitalize.js

--- capitalize.js
+++
@@ -1,7 +0,0 @@
-const capitalize = (str) => {
-  let words = [];
-  for (const word of str.split(" ")) {
-    words.push(`${word.charAt(0).toUpperCase()}${word.slice(1)}`);
-  }
-  return words.join(" ");
-};
